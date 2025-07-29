e530de884a5cb4f59d6d71c70d86c9b520325d74
LanceSissing <lance.sissing@me.com>
2025-07-29 12:13:57 +0100

    Implement custom user registration, account page, and success modal

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_buyer = models.BooleanField(default=True, verbose_name='Buyer')
-    is_seller = models.BooleanField(default=False, verbose_name='Seller')
+    is_seller = models.BooleanField(default=True, verbose_name='Seller')
     is_business = models.BooleanField(default=False, verbose_name='Business')
     # Add more custom fields as needed
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('account')  # Redirect to account page after registration
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+def account(request):
+    return render(request, 'account.html')
