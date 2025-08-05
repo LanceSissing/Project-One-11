54919896593fd23b3229f3bdd123e982bc4ff5e5
LanceSissing <lance.sissing@me.com>
2025-08-05 19:43:50 +0100

    add new message sent functionality

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('inbox/', views.inbox, name='messages_inbox'),
     path('message/<int:message_id>/', views.message_detail, name='message_detail'),
     path('account/edit/', views.edit_account_view, name='edit_account'),
+    path('messages/<int:message_id>/reply/', views.message_reply, name='message_reply'),
+    path('sent/', views.sent_messages, name='sent_messages'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -9,12 +9,39 @@ def inbox(request):
     return render(request, 'marketplace/inbox.html', {'messages': messages})
 
 @login_required
+def sent_messages(request):
+    messages = Message.objects.filter(sender=request.user).order_by('-timestamp')
+    return render(request, 'marketplace/sent_messages.html', {'messages': messages})
+
+@login_required
 def message_detail(request, message_id):
     message = get_object_or_404(Message, pk=message_id, recipient=request.user)
     if not message.is_read:
         message.is_read = True
         message.save()
     return render(request, 'marketplace/message_detail.html', {'message': message})
+
+@login_required
+def message_reply(request, message_id):
+    original_message = get_object_or_404(Message, id=message_id)
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.sender = request.user
+            reply.recipient = original_message.sender
+            reply.item = original_message.item  # Optional: link to the same item
+            reply.save()
+            return redirect('messages_inbox')
+    else:
+        form = MessageForm(initial={
+            'subject': f"Re: {original_message.subject}",
+        })
+    return render(request, 'marketplace/message_reply.html', {
+        'form': form,
+        'original_message': original_message,
+    })
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Item, Message
